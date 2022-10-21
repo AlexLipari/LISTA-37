bc112bbdb304c13a0b54de2d015eebdfc9dc0d8c
Alexandre Lipari <santuslipari@gmail.com>
2022-10-20 23:44:26 -0300

    Lista 37 exercício 3.

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-var base;
-base = "aazefhh453bcc4";
-console.log(CompararIguais(base));
-function CompararIguais(texto) {
-    var resultado = texto.match(/(.)(?=.*\1)/gi);
+// let base: string
+// base = "aazefhh453bcc4"
+// console.log(CompararIguais(base))
+// function CompararIguais(texto: string):number {
+//     let resultado = texto.match(/(.)(?=.*\1)/gi)
+//     return resultado.length
+// }
+var frase;
+frase = "faz parte do meu show";
+console.log(Transformar(frase));
+function Transformar(frase) {
+    var resultado = frase.replace(/(^\w{1})|(\s+\w{1})/g, function (letra) { return letra.toUpperCase(); }).replace(/\s/g, '');
     return resultado;
 }
--- script.ts
+++ script.ts
@@ -1,9 +1,16 @@
-let base: string
-base = "aazefhh453bcc4"
+// let base: string
+// base = "aazefhh453bcc4"
 
-console.log(CompararIguais(base))
+// console.log(CompararIguais(base))
 
-function CompararIguais(texto: string) {
-    let resultado = texto.match(/(.)(?=.*\1)/gi)
+// function CompararIguais(texto: string):number {
+//     let resultado = texto.match(/(.)(?=.*\1)/gi)
+//     return resultado.length
+// }
+let frase: string
+frase = "faz parte do meu show"
+console.log(Transformar(frase))
+function Transformar(frase: string): string {
+    let resultado = frase.replace(/(^\w{1})|(\s+\w{1})/g, letra => letra.toUpperCase()).replace(/\s/g, '');
     return resultado
 }
